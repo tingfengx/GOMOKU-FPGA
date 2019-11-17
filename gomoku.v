98c3dd3d1b776895ff7c308a42f59fc83a2ecabe
TINGFENG XIA <tingfeng.xia@outlook.com>
2019-11-17 11:36:42 -0500

    misc

---
+++ gomoku.v
@@ -0,0 +1,81 @@
+module gomoku(CLOCK_50, 
+			  PS2_CLK, 
+			  PS2_DAT, 
+			  KEY, 
+			  LEDR, 
+			  HEX0, 
+			  HEX1, 
+			  HEX2, 
+			  HEX3, 
+			  HEX4, 
+			  HEX5, 
+			  VGA_CLK, 
+			  VGA_HS, 
+			  VGA_VS, 
+			  VGA_BLANK_N, 
+			  VGA_SYNC_N, 
+			  VGA_R, 
+			  VGA_G, 
+			  VGA_B);
+
+	input CLOCK_50;
+	// special inout for the PS2 data transfer
+	inout PS2_CLK, PS2_DAT;
+	input [3:0] KEY;
+	output [9:0] LEDR;
+	output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+	output VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N;
+	output [9:0] VGA_R, VGA_G, VGA_B;
+	
+	wire resetn;
+	assign resetn = KEY[0];
+
+    vga_adapter VGA(
+		.resetn(resetn),
+		.clock(CLOCK_50),
+		.colour(colour),
+		.x(x),		
+        .y(y),
+		.plot(writeEn),
+		/* Signals for the DAC to drive the monitor. */
+		.VGA_R(VGA_R),
+		.VGA_G(VGA_G),
+		.VGA_B(VGA_B),
+		.VGA_HS(VGA_HS),
+		.VGA_VS(VGA_VS),
+		.VGA_BLANK(VGA_BLANK_N),
+		.VGA_SYNC(VGA_SYNC_N),
+		.VGA_CLK(VGA_CLK));
+	defparam VGA.RESOLUTION = "160x120";
+	defparam VGA.MONOCHROME = "FALSE";
+	defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+	defparam VGA.BACKGROUND_IMAGE = "./img/green_grid_new.mif";
+
+endmodule 
+
+module hex_decoder(hex_digit, segments);
+    input [3:0] hex_digit;
+    output reg [6:0] segments;
+   
+    always @(*)
+        case (hex_digit)
+            4'h0: segments = 7'b100_0000;
+            4'h1: segments = 7'b111_1001;
+            4'h2: segments = 7'b010_0100;
+            4'h3: segments = 7'b011_0000;
+            4'h4: segments = 7'b001_1001;
+            4'h5: segments = 7'b001_0010;
+            4'h6: segments = 7'b000_0010;
+            4'h7: segments = 7'b111_1000;
+            4'h8: segments = 7'b000_0000;
+            4'h9: segments = 7'b001_1000;
+            4'hA: segments = 7'b000_1000;
+            4'hB: segments = 7'b000_0011;
+            4'hC: segments = 7'b100_0110;
+            4'hD: segments = 7'b010_0001;
+            4'hE: segments = 7'b000_0110;
+            4'hF: segments = 7'b000_1110;   
+            default: segments = 7'h7f;
+        endcase
+endmodule
+
