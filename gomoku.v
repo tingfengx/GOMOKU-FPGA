89e49c55bdb400102a36cfb03e354f8d940d57e1
Tingfeng Xia <tingfeng.xia@outlook.com>
2019-11-20 00:23:54 -0500

    add

--- gomoku.v
+++ gomoku.v
@@ -1,34 +1,42 @@
-module gomoku(CLOCK_50, PS2_CLK, PS2_DAT, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_R, VGA_G, VGA_B);
+module gomoku(CLOCK_50, PS2_CLK, PS2_DAT, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_R, VGA_G, VGA_B);
 	input CLOCK_50;
 	inout PS2_CLK, PS2_DAT;
 	input [3:0] KEY;
 	output [9:0] LEDR;
-	output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+	output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4;
 	output VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N;
 	output [9:0] VGA_R, VGA_G, VGA_B;
 	
-	wire resetn = KEY[0];
-	// assign resetn = KEY[0];
-	
-	// wires left right up and down not used; space also not used
+	wire load_x, load_y, load_colour;
+	wire go;
+	wire resetn;
 	wire w, a, s, d, left, right, up, down, space, enter;
-	// game_state 
-	wire game_state;
-	wire game_win_color;
+	wire game_state, game_win_color;
+	wire writeEn;
+	wire [2:0] vga_colour;
 	
-	reg [2:0] in_x = 3'd3
+	
+	reg [2:0] in_x = 3'd3;
 	reg [2:0] in_y = 3'd3;
-	reg in_color = 1'b0; // black starts first
+	reg in_color = 1'b0;
+	reg [7:0] vga_x;
+	reg [6:0] vga_y;
+	
+	
+	assign resetn = KEY[0];
+	assign go = (enter && ~display_board[7*in_x + in_y]) && ~display_game_state;
+	assign writeEn = ((display_game_state && cz_en) && wintxt[cy][cx]) || (go && circle[cy][cx]);
+	
+	localparam wintxt_start_x = 8'd2,
+				  wintxt_start_y = 7'd2;
 	
 	hex_decoder hex0(.hex_digit(in_y), .segments(HEX0)), // y coord
 					hex1(.hex_digit(in_x), .segments(HEX1)), // x coord
 					hex2(.hex_digit(in_color), .segments(HEX2)), // input color
 					hex3(.hex_digit(game_state), .segments(HEX3)), // game state
-					hex4(.hex_digit(game_win_color), .segments(HEX4)), // win color
-					hex5(.hex_digit(board[7*in_x + in_y]), .segments(HEX5)); // stone at game logic board coordinate
+					hex4(.hex_digit(game_win_color), .segments(HEX4)); // win color
 	
-	// keyboard stuff
-	reg left_lock, right_lock, up_lock, down_lock; // lock so it doesnt activate every clock tick
+	reg left_lock, right_lock, up_lock, down_lock; // simulate pulses
 	wire left_key, right_key, up_key, down_key;
 	assign left_key = a && ~left_lock;
 	assign right_key = d && ~right_lock;
@@ -63,38 +71,23 @@ module gomoku(CLOCK_50, PS2_CLK, PS2_DAT, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX
 		end
 	end
 	
-	board7 game_board(
-		.clk(CLOCK_50), 
-		.resetn(resetn), 
-		.go(go), 
-		.x(in_x), 
-		.y(in_y), 
-		.color(in_color), 
-		.board_flat(board_flat), 
-		.state(game_state), 
-		.win_color(game_win_color)
-	);
+	board7 game_board(.clk(CLOCK_50), .resetn(resetn), .go(go), .x(in_x), .y(in_y), .color(in_color), .board_flat(board_flat), .state(game_state), .win_color(game_win_color));
 	
-	reg display_board [7*7-1:0] = 49b'0;
-	// integer j;
-	// initial begin
-	// 	for (j = 0; j < 7*7; j = j + 1)
-	// 		display_board[j] <= 1'b0;
-	// end
+	reg display_board [7*7-1:0];
+	integer j;
+	initial begin
+		for (j = 0; j < 7*7; j = j + 1)
+			display_board[j] <= 1'b0;
+	end
 	
 	wire [7*7*2-1:0] board_flat;
 	wire [1:0] board [7*7-1:0];
 	genvar i;
 	generate for (i = 0; i < 7*7; i = i + 1) begin:unflatten
 		assign board[i] = board_flat[2*i+1:2*i];
-	end endgenerate
-	
-	wire [2:0] vga_colour;
-	reg [7:0] vga_x;
-	reg [6:0] vga_y;
-	wire writeEn;
 	
 	assign vga_colour = (game_state ? game_win_color : in_color) ? 3'b111 : 3'b000;
+	
 	
 	reg [3:0] cx = 4'd0, cy = 4'd0;
 	
@@ -121,12 +114,12 @@ module gomoku(CLOCK_50, PS2_CLK, PS2_DAT, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX
 				cz_en <= 0;
 		end
 	end
-
+	
 	always@(posedge CLOCK_50)
 	begin
 		if (display_game_state) begin
 			cx <= cx + 1;
-			if (cx == 7'd68) begin
+			if (cx == 4'd14) begin
 				cx <= 0;
 				cy <= cy >= 4'd4 ? 0 : cy + 1;
 			end
@@ -144,10 +137,8 @@ module gomoku(CLOCK_50, PS2_CLK, PS2_DAT, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX
 		end
 	end
 	
-	// localparam board_start_x = 8'd31 - 8'd7, // coords to start drawing from
-	// 			  board_start_y = 7'd11 - 7'd7;
-	localparam board_start_x = 8'd24;
-	localparam board_start_y = 7'd4;
+	localparam board_start_x = 8'd31 - 8'd7, // coords to start drawing from
+				  board_start_y = 7'd11 - 7'd7;
 	
 	always@(*)
 	begin
@@ -157,10 +148,8 @@ module gomoku(CLOCK_50, PS2_CLK, PS2_DAT, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX
 				vga_y = wintxt_start_y + cy;
 			end
 			0: begin
-				vga_x = board_start_x + in_x * 8'd16 + cx;
-				vga_y = board_start_y + in_y * 7'd16 + cy;
-				// vga_x = board_start_x + in_x * (8'd15 + 8'd1) + cx;
-				// vga_y = board_start_y + in_y * (7'd15 + 7'd1) + cy;
+				vga_x = board_start_x + in_x * (8'd15 + 8'd1) + cx;
+				vga_y = board_start_y + in_y * (7'd15 + 7'd1) + cy;
 			end
 		endcase
 	end
@@ -184,20 +173,15 @@ module gomoku(CLOCK_50, PS2_CLK, PS2_DAT, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX
 		circle[13] = 15'b000111111111000;
 		circle[14] = 15'b000001111100000;
 	end
-	
-	assign writeEn = ((display_game_state && cz_en) && wintxt[cy][cx]) || (go && circle[cy][cx]);
-	
-	localparam wintxt_start_x = 8'd2, wintxt_start_y = 7'd2;
 				  
-	// text of winnning
-	reg [0:67] wintxt [4:0];
+	reg [0:14] wintxt [4:0];
 	always@(*)
 	begin
-		wintxt[0] = 68'b01110011110111110111100011100100010111101111011001010101110111110011;
-		wintxt[1] = 68'b10001010010101010100000100010100010100001001011001010100100100010011;
-		wintxt[2] = 68'b10000011110101010111100100010100010111101111011001010100100100010011;
-		wintxt[3] = 68'b10111010010101010100000100010010100100001010000001010100100100010000;
-		wintxt[4] = 68'b01101010010101010111100011100001000111101001011000101001110100010011;
+		wintxt[0] = 15'b101010111010001;
+		wintxt[1] = 15'b101010010011001;
+		wintxt[2] = 15'b101010010010101;
+		wintxt[3] = 15'b101010010010011;
+		wintxt[4] = 15'b010100111010001;
 	end
 	
 	keyboard_tracker #(.PULSE_OR_HOLD(0)) keyboard(.clock(CLOCK_50), .reset(resetn),
@@ -214,14 +198,12 @@ module gomoku(CLOCK_50, PS2_CLK, PS2_DAT, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX
 	defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
 	defparam VGA.BACKGROUND_IMAGE = "img/board.colour.mif";
 	
-
-
 	always@(negedge go)
 	begin
 		display_board[7*in_x + in_y] <= 1'b1;
 		in_color <= !in_color;
 	end
-
+	
 endmodule
 
 module board7(clk, resetn, go, x, y, color, board_flat, state, win_color);
@@ -260,19 +242,19 @@ module board7(clk, resetn, go, x, y, color, board_flat, state, win_color);
 	
 	wire [(9+6*4)-1:0] q;
 	
-	genvar k, m;
-	generate for (k = 3'd2; k <= 3'd4; k = k + 3'd1) begin: nodes_outer
-		for (m = 3'd2; m <= 3'd4; m = m + 3'd1) begin: nodes_inner
-			centernode cn(.x(k), .y(m), .board_flat(board_flat), .q(q[3*(k - 2) + (m - 2)]));
-		end
-		edgenode en_x0(.x(3'd0), .y(k), .board_flat(board_flat), .q(q[9 + (k - 2)]));
-		edgenode en_x1(.x(3'd1), .y(k), .board_flat(board_flat), .q(q[12 + (k - 2)]));
-		edgenode en_x2(.x(3'd5), .y(k), .board_flat(board_flat), .q(q[15 + (k - 2)]));
-		edgenode en_x3(.x(3'd6), .y(k), .board_flat(board_flat), .q(q[18 + (k - 2)]));
-		edgenode en_y0(.x(k), .y(3'd0), .board_flat(board_flat), .q(q[21 + (k - 2)]));
-		edgenode en_y1(.x(k), .y(3'd1), .board_flat(board_flat), .q(q[24 + (k - 2)]));
-		edgenode en_y2(.x(k), .y(3'd5), .board_flat(board_flat), .q(q[27 + (k - 2)]));
-		edgenode en_y3(.x(k), .y(3'd6), .board_flat(board_flat), .q(q[30 + (k - 2)]));
+	genvar a;
+	generate for (a = 3'd2; a <= 3'd4; a = a + 3'd1) begin: all_nodes
+		centernode cn0(.x(a), .y(3'd2), .board_flat(board_flat), .q(q[3*(a - 2) + 0]));
+		centernode cn1(.x(a), .y(3'd3), .board_flat(board_flat), .q(q[3*(a - 2) + 1]));
+		centernode cn2(.x(a), .y(3'd4), .board_flat(board_flat), .q(q[3*(a - 2) + 2]));
+		edgenode en_y0(.x(a), .y(3'd0), .board_flat(board_flat), .q(q[21 + (a - 2)]));
+		edgenode en_y1(.x(a), .y(3'd1), .board_flat(board_flat), .q(q[24 + (a - 2)]));
+		edgenode en_y2(.x(a), .y(3'd5), .board_flat(board_flat), .q(q[27 + (a - 2)]));
+		edgenode en_y3(.x(a), .y(3'd6), .board_flat(board_flat), .q(q[30 + (a - 2)]));
+		edgenode en_x0(.x(3'd0), .y(a), .board_flat(board_flat), .q(q[9 + (a - 2)]));
+		edgenode en_x1(.x(3'd1), .y(a), .board_flat(board_flat), .q(q[12 + (a - 2)]));
+		edgenode en_x2(.x(3'd5), .y(a), .board_flat(board_flat), .q(q[15 + (a - 2)]));
+		edgenode en_x3(.x(3'd6), .y(a), .board_flat(board_flat), .q(q[18 + (a - 2)]));
 	end endgenerate
 	
 	assign state = |q;
@@ -288,21 +270,22 @@ module centernode(x, y, board_flat, q);
 	input [2:0] x, y;
 	input [7*7*2-1:0] board_flat;
 	output wire [1:0] q;
-	wire h, v, dil, dir;
-	node cn(.x(x), .y(y), .board_flat(board_flat), .h(h), .v(v), .dil(dil), .dir(dir));
-	assign q = (x >= 2 && x <= 4 && y >= 2 && y <= 4) && (h || v || dil || dir);
+	wire horizontal, vertical, diag_left, diag_right;
+	node cn(.x(x), .y(y), .board_flat(board_flat), .horizontal(horizontal), .vertical(vertial), .diag_left(diag_left), .diag_right(diag_right));
+	wire win_one_way = | {horizontal, vertical, diag_left, diag_right};
+	assign q = x >= 2 && x <= 4 && y >= 2 && y <= 4 && win_one_way;
 endmodule
 
 module edgenode(x, y, board_flat, q);
 	input [2:0] x, y;
 	input [7*7*2-1:0] board_flat;
 	output wire [1:0] q;
-	wire h, v, dil, dir;
-	node cn(.x(x), .y(y), .board_flat(board_flat), .h(h), .v(v), .dil(dil), .dir(dir));
-	assign q = ((x < 2 || x > 4) && v) || ((y < 2 || y > 4) && h);
+	wire horizontal, vertical, diag_left, diag_right;
+	node cn(.x(x), .y(y), .board_flat(board_flat), .horizontal(horizontal), .vertical(vertial), .diag_left(diag_left), .diag_right(diag_right));
+	assign q = ((x < 2 || x > 4) && (vertical || horizontal));
 endmodule
 
-module node(x, y, board_flat, h, v, dil, dir);
+module node(x, y, board_flat, horizontal, vertical, diag_left, diag_right);
 	input [2:0] x, y;
 	input [7*7*2-1:0] board_flat;
 	wire [1:0] board [7*7-1:0];
@@ -310,32 +293,34 @@ module node(x, y, board_flat, h, v, dil, dir);
 	generate for (i = 0; i < 7*7; i = i + 1) begin:unflatten
 		assign board[i] = board_flat[2*i+1:2*i];
 	end endgenerate
-	reg [1:0] c, l, l2, r, r2, u, u2, d, d2, ul, ul2, ur, ur2, dl, dl2, dr, dr2;
-	output reg h, v, dil, dir;
+	
+	output reg horizontal, vertical, diag_left, diag_right;
 	always@(*)
-	begin
-		c = board[7*x + y];
-		l = board[7*(x - 1) + y ];	
-		l2 = board[7*(x - 2) + y];
-		r = board[7*(x + 1) + y];
-		r2 = board[7*(x + 2) + y];
-		u = board[7*x + y - 1];
-		u2 = board[7*x + y - 2];
-		d = board[7*x + y + 1];
-		d2 = board[7*x + y + 2];
-		ul = board[7*(x - 1) + y - 1];
-		ul2 = board[7*(x - 2) + y - 2];
-		ur = board[7*(x + 1) + y - 1];
-		ur2 = board[7*(x + 2) + y - 2];
-		dl = board[7*(x - 1) + y + 1];
-		dl2 = board[7*(x - 2) + y + 2];
-		dr = board[7*(x + 1) + y + 1];
-		dr2 = board[7*(x + 2) + y + 2];
-		
-		h = c > 0 && l == c && l2 == c && r == c && r2 == c;
-		v = c > 0 && u == c && u2 == c && d == c && d2 == c;
-		dil = c > 0 && ul == c && ul2 == c && dr == c && dr2 == c;
-		dir = c > 0 && ur == c && ur2 == c && dl == c && dl2 == c;
+	begin		
+		horizontal = (board[7*x + y] > 0 
+			&& board[7*(x - 1) + y] == board[7*x + y] 
+			&& board[7*(x - 2) + y] == board[7*x + y] 
+			&& board[7*(x + 1) + y] == board[7*x + y] 
+			&& board[7*(x + 2) + y] == board[7*x + y]
+		);
+		vertical = (board[7*x + y] > 0 
+			&& board[7*x + y - 1] == board[7*x + y] 
+			&& board[7*x + y - 2] == board[7*x + y] 
+			&& board[7*x + y + 1] == board[7*x + y] 
+			&& board[7*x + y + 2] == board[7*x + y]
+		);
+		diag_left = (board[7*x + y] > 0 
+			&& board[7*(x - 1) + y - 1] == board[7*x + y] 
+			&& board[7*(x - 2) + y - 2] == board[7*x + y] 
+			&& board[7*(x + 1) + y + 1] == board[7*x + y] 
+			&& board[7*(x + 2) + y + 2] == board[7*x + y]
+		);
+		diag_right = (board[7*x + y] > 0 
+			&& board[7*(x + 1) + y - 1] == board[7*x + y] 
+			&& board[7*(x + 2) + y - 2] == board[7*x + y] 
+			&& board[7*(x - 1) + y + 1] == board[7*x + y] 
+			&& board[7*(x - 2) + y + 2] == board[7*x + y]
+		);
 	end
 endmodule
 
@@ -363,5 +348,4 @@ module hex_decoder(hex_digit, segments);
             4'hF: segments = 7'b000_1110;   
             default: segments = 7'h7f;
         endcase
-endmodule
-
+endmodule 
